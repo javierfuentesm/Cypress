59542b79d17d52916cb1b3468d13155542cc1210
Javier Fuentes Mora <roberto@hudsonmx.com>
2020-08-19 14:05:07 -0500

    Se mejora

--- cypress/integration/login/login_steps.js
+++ cypress/integration/login/login_steps.js
@@ -1,18 +1,31 @@
-import { Given ,Then} from "cypress-cucumber-preprocessor/steps";
-require('cypress-xpath')
+import { Given, Then } from "cypress-cucumber-preprocessor/steps";
+require("cypress-xpath");
+
+Cypress.Commands.add(
+  "iframe",
+  { prevSubject: "element" },
+  ($iframe, callback = () => {}) => {
+    cy.log("Getting iframe body");
+    return cy
+      .wrap($iframe)
+      .should((iframe) => expect(iframe.contents().find("body")).to.exist)
+      .then((iframe) => cy.wrap(iframe.contents().find("body")))
+      .within({}, callback);
+  }
+);
 
 Given(`I am in the right page`, () => {
-    cy.visit('/')
+  cy.visit("/");
 });
 
+Then("I should fill the inputs", () => {
+  cy.get("#username").type("admin@danhc.com");
+  cy.get(".btn").click();
+  cy.get("#password").type("user");
+  cy.get(".login-btn").click();
+  cy.wait(15000);
 
-Then('I should fill the inputs',  ()=> {
-    cy.get('#username').type('admin@danhc.com')
-    cy.get('.btn').click()
-    cy.get('#password').type('user')
-    cy.get('.login-btn').click()
-    cy.wait(15000)
-
+  /* 
     cy.get('iframe')
     .then(($iframe) => {
       const $body = $iframe.contents().find('body')
@@ -23,8 +36,8 @@ Then('I should fill the inputs',  ()=> {
         cy.wait(5000)
 
     
-    })
-
+    }) */
+  /* 
     cy.get('iframe')
     .then(($iframe) => {
       const $body = $iframe.contents().find('body')  
@@ -33,5 +46,18 @@ Then('I should fill the inputs',  ()=> {
       .click()
 
     
-    })
+    }) */
+  cy.get("iframe").iframe(() => {
+    cy.xpath(
+      "/html/body/app-root/fury-layout/mat-drawer-container/mat-drawer-content/div/div/div/app-dashboard/div/div/app-dashboard-sections/div/div[2]/div/a[1]"
+    ).click();
+  });
+  
+  cy.wait(7000);
+
+  cy.get("iframe").iframe(() => {
+    cy.xpath(
+      '//*[@id="content-wrapper"]/hmx-master-data-root/div/hmx-nav-tab-container/div/div[1]/hmx-tabs/hmx-tab[2]'
+    ).click();
+  });
 });
